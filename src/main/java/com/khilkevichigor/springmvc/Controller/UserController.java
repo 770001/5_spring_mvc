c75d81b2b71e7916538c7066c49b433d9e03de8a
igor <khilkevichigor@gmail.com>
2019-10-13 23:53:21 +0300

    fix controller and jsp

--- src/main/java/com/khilkevichigor/springmvc/Controller/UserController.java
+++ src/main/java/com/khilkevichigor/springmvc/Controller/UserController.java
@@ -3,11 +3,9 @@ package com.khilkevichigor.springmvc.Controller;
 import com.khilkevichigor.springmvc.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import com.khilkevichigor.springmvc.service.UserService;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,8 +20,8 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(value = "read", method = RequestMethod.GET)
     //маппимся на ссылку read, метод GET для получения списка всех юзеров, аналог ReadServlet
+    @RequestMapping(value = "read", method = RequestMethod.GET)
     public String allUsers(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("allUsers", this.userService.getAllUsers());
@@ -32,7 +30,7 @@ public class UserController {
 
     //получаем страницу create.jsp
     @RequestMapping(value = "create", method = RequestMethod.GET)
-    public ModelAndView addPage(@ModelAttribute("message") String message) {
+    public ModelAndView addPage() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("create");
         return modelAndView;
@@ -49,16 +47,30 @@ public class UserController {
         return "redirect:/read";
     }
 
-    @RequestMapping(value = "delete/{id}")
-    public String deleteUser(@PathVariable("id") long id) {
-        this.userService.deleteUser(id);
-        return "redirect:/read";
+    //получаем страницу update.jsp
+    @RequestMapping(value = "update", method = RequestMethod.GET)
+    public ModelAndView updatePage(@RequestParam("id") long id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("update");
+        modelAndView.addObject("user", this.userService.getUserById(id));
+        return modelAndView;
     }
 
-    @RequestMapping(value = "update/{id}")
-    public String updateUser(@PathVariable("id") long id, Model model) {
-        model.addAttribute("user", this.userService.getUserById(id));
-        model.addAttribute("allUsers", this.userService.getAllUsers());
-        return "redirect:/read";
+    //обновляем юзера
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public ModelAndView updateUser(@ModelAttribute("user") User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/read");
+        userService.updateUser(user);
+        return modelAndView;
+    }
+
+    //удаляем юзера
+    @RequestMapping(value = "delete", method = RequestMethod.GET)
+    public ModelAndView deleteUser(@RequestParam("id") long id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/read");
+        userService.deleteUser(id);
+        return modelAndView;
     }
 }
