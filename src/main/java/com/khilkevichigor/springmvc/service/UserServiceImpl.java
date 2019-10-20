2e0623e5f0b692ab628df7575c585700aa25573e
igor <khilkevichigor@gmail.com>
2019-10-20 20:22:42 +0300

    @Transactional повесил на класс новый .giitignore

--- src/main/java/com/khilkevichigor/springmvc/service/UserServiceImpl.java
+++ src/main/java/com/khilkevichigor/springmvc/service/UserServiceImpl.java
@@ -22,35 +22,30 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-//    @Transactional //класс поддерживает транзакции
     //взять всех юзеров
     public List<User> getAllUsers() {
         return this.userDAO.getAllUsers();
     }
 
     @Override
-//    @Transactional
     //взять юзера по id
     public User getUserById(long id) {
         return this.userDAO.getUserById(id);
     }
 
     @Override
-//    @Transactional
     //добавить юзера
     public void addUser(User user) {
         this.userDAO.addUser(user);
     }
 
     @Override
-//    @Transactional
     //обновить юзера
     public void updateUser(User user) {
         this.userDAO.updateUser(user);
     }
 
     @Override
-//    @Transactional
     //удалить юзера
     public void deleteUser(long id) {
         this.userDAO.deleteUser(id);
